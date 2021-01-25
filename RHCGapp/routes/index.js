7348963f13fbc61e846be948df875707bddf492d
Sebastian Krawiec <sebastian.arielk@gmail.com>
2021-01-25 12:13:09 -0300

    adding controllers

--- RHCGapp/routes/index.js
+++ RHCGapp/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+let indexController = require("../controllers/indexController");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index);
 
 module.exports = router;
