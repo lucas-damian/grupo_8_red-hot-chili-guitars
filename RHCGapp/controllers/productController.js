c67f3cbf791b91b14b8130e424a08efc472bf39a
Sebastian Krawiec <sebastian.arielk@gmail.com>
2021-02-20 02:49:46 -0300

    adding products list

--- RHCGapp/controllers/productController.js
+++ RHCGapp/controllers/productController.js
@@ -4,16 +4,17 @@ const Db_products = "./data/productos.json";
 module.exports = {
     cargaProduc:(req,res) =>{
         res.render("cargaProducto", {
-            title: "Producto",
+            title: "cargando instrumento",
         });
     },
 
     listar: (req,res) => {
        let productos = JSON.parse(fs.readFileSync(Db_products,"utf-8"));
-       res.send(productos)
-       res.render("listado",{
-           productos
-       })
+     
+       res.render("adminProducts",{
+           title:"productos",
+           productos:productos
+       });
     },
 
     formDirect: (req,res) => {
