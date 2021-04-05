beeb34e5b51f5ffb1ce015237b2e226de993de21
Sebastian Krawiec <sebastian.arielk@gmail.com>
2021-04-05 12:03:09 -0300

    fixing front validator on log-in

--- RHCGapp/public/javascript/logRegValidator.js
+++ RHCGapp/public/javascript/logRegValidator.js
@@ -1,7 +1,6 @@
 let qs = function(element){
-   return document.querySelector(element);
-}
-
+    return document.querySelector(element);
+ }
 
 let $formRegister = qs("#formRegister"),
     $userName = qs('#userName'),
@@ -15,10 +14,18 @@ let $formRegister = qs("#formRegister"),
     
     $pass2 = qs('#pass2-Register'),
     $pass2Errors = qs('#pass2Errors'),
+
+
+    $user=qs('#user'),
+    $userErrors=qs('#userErrors'),
+
+    $password=qs('#password'),
+    $passwordErrors=qs('#passwordErrors')
     
     regExAlpha= /^[a-zA-Z\sñáéíóúü ]*$/,
     regExEmail= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
     regExPass= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+
 
 
 window.onload = function() {
@@ -27,15 +34,15 @@ window.onload = function() {
         
         switch (true) {
             
-            case !this.value.trim():
-            $nameErrors.innerHTML += 'debes ingresar un usuario';
-            $userName.classList.add('is-invalid')
+            case !$userName.value.trim():
+            $nameErrors.innerHTML = 'debes ingresar un usuario';
+            $userName.classList.add('is-invalid');
             break;
             
             
             case !regExAlpha.test($userName.value):
             $nameErrors.innerHTML = 'el usuario no puede llevar simbolos';
-            $userName.classList.add('is-invalid')  
+            $userName.classList.add('is-invalid');
             break; 
             
             
@@ -52,12 +59,14 @@ window.onload = function() {
         switch (true) {
             case !this.value.trim():
             $emailErrors.innerHTML = 'el email es requerido';
-            $emailRegister.classList.add('is-invalid')
+            $emailRegister.classList.add('is-invalid');
             break;
+            
             case !regExEmail.test($emailRegister.value):
             $emailErrors.innerHTML = 'el email no puede llevar mayusculas y siempre debe que tener @';
-            $emailRegister.classList.add('is-invalid')
+            $emailRegister.classList.add('is-invalid');
             break
+            
             default:
             $emailRegister.classList.remove('is-invalid');
             $emailRegister.classList.add('is-valid');
@@ -71,12 +80,12 @@ window.onload = function() {
         switch (true) {
             case !this.value.trim():
             $passErrors.innerHTML = 'Es necesario una contraseña';
-            $pass.classList.add('is-invalid')
+            $pass.classList.add('is-invalid');
             break;
             
             case !regExPass.test($pass.value):
             $passErrors.innerHTML = 'Minimo 3 caracteres y máximo 6 caracteres con letras minusculasy mayusculas';
-            $pass.classList.add('is-invalid')
+            $pass.classList.add('is-invalid');
             break
             
             default:
@@ -106,6 +115,55 @@ window.onload = function() {
             break;
         }
 
+    })
+
+/* 
+        ////// LOGUEO
+
+*/
+
+    $user.addEventListener("blur", function(){
+        switch (true) {
+        
+            case !this.value.trim():
+            $userErrors.innerHTML = 'Debes ingresar el email de un usuario';
+            $user.classList.add('is-invalid')
+            break;
+            
+            
+            case !regExEmail.test($user.value):
+            $userErrors.innerHTML = 'el email esta mal escrito';
+            $user.classList.add('is-invalid')  
+            break; 
+             
+            
+            default:
+            $userErrors.innerHTML = " ";
+            $user.classList.remove('is-invalid');
+            $user.classList.add('is-valid');
+            break;
+        }
+    });
+    
+    $password.addEventListener("blur", function(){
+        switch (true) {
+        
+            case !this.value.trim():
+            $passwordErrors.innerHTML = 'Escribe la contraseña';
+            $password.classList.add('is-invalid')
+            break;
+            
+            case !regExPass.test($password.value):
+            $passwordErrors.innerHTML = 'Minimo 3 caracteres y máximo 6 caracteres con letras minusculasy mayusculas';
+            $password.classList.add('is-invalid')
+            break;
+            
+            default:
+            $password.classList.remove('is-invalid');
+            $password.classList.add('is-valid');
+            $passwordErrors.innerHTML = ''
+            break;
+        }
     })
 
 }
--- RHCGapp/public/javascript/sandbox.js
+++ RHCGapp/public/javascript/sandbox.js
@@ -1,3 +1,5 @@
+
+
 const menuBtn = document.querySelectorAll(".menu-btn");
 const barra = document.getElementById("barra");
 
@@ -6,69 +8,33 @@ const extendedBtn = document.querySelectorAll(".carrito-extended");
 
 const botonRegister = document.querySelector('.btn--register')
 
-for(let i = 0; i < menuBtn.length ; i++){
-    menuBtn[i].addEventListener("click", () => {
-        barra.classList.toggle("active");
-    })
-}
 
 
+window.onload = function () {
 
-
-
-for(let i = 0; i < carritoBtn.length ; i++){
-    carritoBtn[i].addEventListener("click", () => {
-         for(let j = 0; j < extendedBtn.length ; j++){
-            extendedBtn[j].classList.toggle("active");
-        } 
-    })
-} 
-
-window.addEventListener('load',{
-    function(event){
-        botonRegister.preventDefault(event)
+    for(let i = 0; i < menuBtn.length ; i++){
+        menuBtn[i].addEventListener("click", () => {
+            barra.classList.toggle("active");
+        })
     }
-})
-
-/* let carritoBtn = document.getElementById("c1");
-
-let extendedBtn = document.getElementById("1");
-console.log("normal" + extendedBtn)
-
-
-extendedBtn.id++
-
-console.log("incrementado" + extendedBtn) */
-
-
-/* for(let i = 0; i < carritoBtn.length ; i++){
-    carritoBtn[i].addEventListener("click", () => {
-
-        for(let j = 0; j < extendedBtn.length ; j++){
-            extendedBtn[j].classList.toggle("active");
-            extendedBtn.id++ ;
-            console.log("extended :" + extendedBtn)
+    
+    
+    
+    
+    
+    for(let i = 0; i < carritoBtn.length ; i++){
+        carritoBtn[i].addEventListener("click", () => {
+             for(let j = 0; j < extendedBtn.length ; j++){
+                extendedBtn[j].classList.toggle("active");
+            } 
+        })
+    } 
+    
+    window.addEventListener('load',{
+        function(event){
+            botonRegister.preventDefault(event)
         }
-
-        carritoBtn = carritoBtn++;
-        console.log("carrito :" + carritoBtn)
     })
-} */
 
-/* for(let i = 0; i < carritoBtn.length ; i++){
-    carritoBtn[i].addEventListener("click", () => {
-        extendedBtn2.classList.toggle("active");
-        /* for(let j = 0; j < extendedBtn.length ; j++){
-            extendedBtn[j].classList.toggle("active");
-        } 
-    })
-} */
 
-/* for(let i = 0; i < carritoBtn.length ; i++){
-    carritoBtn[i].addEventListener("click", () => {
-        extendedBtn3.classList.toggle("active");
-        /* for(let j = 0; j < extendedBtn.length ; j++){
-            extendedBtn[j].classList.toggle("active");
-        } 
-    })
-} */
+}
